b92ab07eed12ec93f8c6eda25bafdf34ab726151
CamWhiting <cameron.whiting@hotmail.com>
2023-01-24 14:48:42 +0800

    Undo config.ts

--- .tina/config.ts
+++ .tina/config.ts
@@ -9,44 +9,20 @@ export default defineConfig({
   token: "e52d5de14710189649490966aceccf5cc69dd169", // Get this from tina.io
   build: {
     outputFolder: "admin",
-    publicFolder: "/",
+    publicFolder: "public",
   },
   media: {
     tina: {
-      mediaRoot: "/assets/images",
-      publicFolder: "",
+      mediaRoot: "",
+      publicFolder: "public",
     },
   },
   schema: {
     collections: [
       {
-        name: "exhibitions",
-        label: "Exhibitions",
-        path: "_exhibitions/2023",
-        //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
-        format: "md",
-        // defaultItem: () => {
-        //   return {
-        //     // Return a default title and the current date as the default date
-        //     layout: 'page',
-        //   }
-        // },
-        // As Jekyll posts require a date at the beginning, this code automatically adds it.
-        ui: {
-          filename: {
-            readonly: false,
-            slugify: values => {
-              const date = new Date();
-              const day = date.getDate();
-              const month = date.getMonth() + 1;
-              const year = date.getFullYear();
-        
-              let currentDate = `${year}-${month}-${day}`;
-        
-              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
-            }
-          }
-        },
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
         fields: [
           {
             type: "string",
@@ -55,55 +31,6 @@ export default defineConfig({
             isTitle: true,
             required: true,
           },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "equipment",
-        label: "Equipment",
-        path: "_equipment",
-        format: "md",
-        // defaultItem: () => {
-        //  return {
-        //     layout: "page",
-        //     grand_parent: "Equipment"
-        //  }
-        // },
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "parent",
-            label: "Sub Category",
-            required: true,
-            list: true,
-            options: [
-              {
-                value: "Fabrication",
-                label: "Fabrication"
-              },
-              {
-                value: "Multimedia",
-                label: "Multimedia"
-              },
-              {
-                value: "Other",
-                label: "Other"
-              },
-            ],
-          },
-
           {
             type: "rich-text",
             name: "body",
@@ -115,3 +42,9 @@ export default defineConfig({
     ],
   },
 });
+
+
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
