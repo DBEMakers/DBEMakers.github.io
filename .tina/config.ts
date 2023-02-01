6c4a2198ede887cfb0194485dcb2c2bb9294803e
CamWhiting <cameron.whiting@hotmail.com>
2023-02-02 07:49:11 +0800

    Reinstall of Tina.io

--- .tina/config.ts
+++ .tina/config.ts
@@ -17,101 +17,101 @@ export default defineConfig({
       publicFolder: "./",
     },
   },
-  schema: {
-    collections: [
-      {
-        name: "exhibitions",
-        label: "Exhibitions",
-        path: "_exhibitions/2023",
-        //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
-        format: "md",
-        // defaultItem: () => {
-        //   return {
-        //     // Return a default title and the current date as the default date
-        //     layout: 'page',
-        //   }
-        // },
-        // As Jekyll posts require a date at the beginning, this code automatically adds it.
-        ui: {
-          filename: {
-            readonly: false,
-            slugify: values => {
-              const date = new Date();
-              const day = date.getDate();
-              const month = date.getMonth() + 1;
-              const year = date.getFullYear();
-        
-              let currentDate = `${year}-${month}-${day}`;
-        
-              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
-            }
+schema: {
+  collections: [
+    {
+      name: "exhibitions",
+      label: "Exhibitions",
+      path: "_exhibitions/2023",
+      //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
+      format: "md",
+      // defaultItem: () => {
+      //   return {
+      //     // Return a default title and the current date as the default date
+      //     layout: 'page',
+      //   }
+      // },
+      // As Jekyll posts require a date at the beginning, this code automatically adds it.
+      ui: {
+        filename: {
+          readonly: false,
+          slugify: values => {
+            const date = new Date();
+            const day = date.getDate();
+            const month = date.getMonth() + 1;
+            const year = date.getFullYear();
+      
+            let currentDate = `${year}-${month}-${day}`;
+      
+            return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
           }
+        }
+      },
+      fields: [
+        {
+          type: "string",
+          name: "title",
+          label: "Title",
+          isTitle: true,
+          required: true,
         },
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-      {
-        name: "equipment",
-        label: "Equipment",
-        path: "_equipment",
-        format: "md",
-        // defaultItem: () => {
-        //  return {
-        //     layout: "page",
-        //     grand_parent: "Equipment"
-        //  }
-        // },
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "string",
-            name: "parent",
-            label: "Sub Category",
-            required: true,
-            list: true,
-            options: [
-              {
-                value: "Fabrication",
-                label: "Fabrication"
-              },
-              {
-                value: "Multimedia",
-                label: "Multimedia"
-              },
-              {
-                value: "Other",
-                label: "Other"
-              },
-            ],
-          },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        },
+      ],
+    },
+    {
+      name: "equipment",
+      label: "Equipment",
+      path: "_equipment",
+      format: "md",
+      // defaultItem: () => {
+      //  return {
+      //     layout: "page",
+      //     grand_parent: "Equipment"
+      //  }
+      // },
+      fields: [
+        {
+          type: "string",
+          name: "title",
+          label: "Title",
+          isTitle: true,
+          required: true,
+        },
+        {
+          type: "string",
+          name: "parent",
+          label: "Sub Category",
+          required: true,
+          list: true,
+          options: [
+            {
+              value: "Fabrication",
+              label: "Fabrication"
+            },
+            {
+              value: "Multimedia",
+              label: "Multimedia"
+            },
+            {
+              value: "Other",
+              label: "Other"
+            },
+          ],
+        },
 
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+        {
+          type: "rich-text",
+          name: "body",
+          label: "Body",
+          isBody: true,
+        },
+      ],
+    },
+  ],
+},
 });
