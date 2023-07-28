32fa7f3090a853f12468fb5aed08a39775f58669
Amy <116771511+DBEMakers@users.noreply.github.com>
2023-07-28 11:12:57 +0800

    updates to tina config
    still broken!

--- .tina/__generated__/config.prebuild.jsx
+++ .tina/__generated__/config.prebuild.jsx
@@ -1,335 +1,278 @@
-import React from 'react';
+// .tina/config.tsx
+import React from "react";
 import { defineConfig } from "tinacms";
-
-// Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-
-export default defineConfig({
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
   branch,
-  clientId: "abe73f23-d496-4c00-a498-d82925b497a2", // Get this from tina.io
-  token: "ff985be20f9172b068d585af5f08124036430979", // Get this from tina.io
+  clientId: "abe73f23-d496-4c00-a498-d82925b497a2",
+  // Get this from tina.io
+  token: "ff985be20f9172b068d585af5f08124036430979",
+  // Get this from tina.io
   build: {
     outputFolder: "admin",
-    publicFolder: "./",
+    publicFolder: "./"
   },
   media: {
     tina: {
       mediaRoot: "/assets/images",
-      publicFolder: "./",
-    },
+      publicFolder: "./"
+    }
   },
-schema: {
-  collections: [
-    //test main pages
-    {
-      name: "mainpage",
-      label: "Main pages",
-      path: "/",
-      match: {
-        include:'*',
-        exclude:'markdown kitchen sink',
-      },
-      //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
-      format: "md",
-      // As Jekyll posts require a date at the beginning, this code automatically adds it.
-      defaultItem: () => {
-        return {
-          layout: 'page',
-        }
-      },
-      ui: {
-        filename: {
-          readonly: false,
-          slugify: values => {
-            const date = new Date();
-            const day = date.getDate();
-            const month = date.getMonth() + 1;
-            const year = date.getFullYear();
-      
-            let currentDate = `${year}-${month}-${day}`;
-      
-            return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+  schema: {
+    collections: [
+      {
+        name: "exhibitions",
+        label: "Exhibitions",
+        path: "_exhibitions",
+        //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
+        format: "md",
+        // As Jekyll posts require a date at the beginning, this code automatically adds it.
+        defaultItem: () => {
+          return {
+            grand_parent: "Exhibitions",
+            layout: "page"
+          };
+        },
+        ui: {
+          filename: {
+            readonly: false,
+            slugify: (values) => {
+              const date = /* @__PURE__ */ new Date();
+              const day = date.getDate();
+              const month = date.getMonth() + 1;
+              const year = date.getFullYear();
+              let currentDate = `${year}-${month}-${day}`;
+              return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, "-")}`;
+            }
+          }
+        },
+        fields: [
+          {
+            name: "layout",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
+            }
           },
-        },
-      },
-      fields: [
-        {
-          type: "string",
-          name: "title",
-          label: "Title",
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: "rich-text",
-          name: "body",
-          label: "Body",
-          isBody: true,
-        },
-      ],
-    },
-    //end test
-
-    //exhibitions
-    {
-      name: "exhibitions",
-      label: "Exhibitions",
-      path: "_exhibitions",
-      match: {
-        include: "*",
-      },
-      //  Named 'md' and not 'markdown' as that is a depracated with Tina.io. Using 'markdown' breaks some of the features such as adding content to the body
-      format: "md",
-      // As Jekyll posts require a date at the beginning, this code automatically adds it.
-      defaultItem: () => {
-        return {
-          grand_parent: 'Exhibitions',
-          layout: 'page',
-        }
-      },
-      ui: {
-        filename: {
-          readonly: false,
-          slugify: values => {
-            const date = new Date();
-            const day = date.getDate();
-            const month = date.getMonth() + 1;
-            const year = date.getFullYear();
-      
-            let currentDate = `${year}-${month}-${day}`;
-      
-            return `${currentDate}-${values?.title?.toLowerCase().replace(/ /g, '-')}`
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
           },
-        },
+          {
+            type: "string",
+            name: "parent",
+            label: "Year",
+            required: true,
+            options: [
+              {
+                value: "2022",
+                label: "2022"
+              },
+              {
+                value: "2023",
+                label: "2023"
+              }
+            ]
+          },
+          {
+            name: "grand_parent",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
+            }
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
       },
-      fields: [
-        {
-          name: "layout",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
+      // Makerspace
+      {
+        name: "makerspace_equipment",
+        label: "Makerspace Equipment",
+        path: "_equipment/makerspace",
+        format: "md",
+        defaultItem: () => {
+          return {
+            grand_parent: "makerspace_equipment",
+            layout: "page"
+          };
+        },
+        fields: [
+          {
+            name: "layout",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
             }
-          }
-        },
-        {
-          type: "string",
-          name: "title",
-          label: "Title",
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: "string",
-          name: "parent",
-          label: "Year",
-          required: true,
-          options: [
-            {
-              value: "2022",
-              label: "2022"
-            },
-            {
-              value: "2023",
-              label: "2023"
-            },
-          ],
-        },
-        {
-          name: "grand_parent",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
+          },
+          {
+            name: "grand_parent",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
             }
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "parent",
+            label: "Sub Category",
+            required: true,
+            options: [
+              {
+                value: "Fabrication",
+                label: "Fabrication"
+              },
+              {
+                value: "Multimedia",
+                label: "Multimedia"
+              },
+              {
+                value: "Other",
+                label: "Other"
+              }
+            ]
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
           }
-        },
-        {
-          type: "rich-text",
-          name: "body",
-          label: "Body",
-          isBody: true,
-        },
-      ],
-    },
-
-    // Makerspace
-    {
-      name: "makerspace_equipment",
-      label: "Makerspace pages",
-      path: "makerspace",
-      match: {
-        include: '*',
+        ]
       },
-      format: "md",
-      defaultItem: () => {
-        return {
-          grand_parent: 'makerspace_equipment',
-          layout: 'page',
-        }
+      // Wood and Metal Workshop
+      {
+        name: "workshop_equipment",
+        label: "Workshop Equipment",
+        path: "_equipment/workshop",
+        format: "md",
+        defaultItem: () => {
+          return {
+            grand_parent: "Workshop 202",
+            layout: "page"
+          };
+        },
+        fields: [
+          {
+            name: "layout",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
+            }
+          },
+          {
+            name: "grand_parent",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
+            }
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "string",
+            name: "parent",
+            label: "Sub Category",
+            required: true,
+            options: [
+              {
+                value: "metal",
+                label: "Metal Working"
+              },
+              {
+                value: "wood",
+                label: "Wood Working"
+              }
+            ]
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
+          }
+        ]
       },
-      fields: [
-        {
-          name: "layout",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
+      // Fashion
+      {
+        name: "fashion_equipment",
+        label: "Fashion",
+        path: "_equipment/fashion",
+        format: "md",
+        defaultItem: () => {
+          return {
+            parent: "Fashion 212",
+            layout: "page"
+          };
+        },
+        fields: [
+          {
+            name: "layout",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
             }
-          }
-        },
-        {
-          name: "grand_parent",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
+          },
+          {
+            name: "parent",
+            type: "string",
+            ui: {
+              component: () => {
+                return React.createElement(React.Fragment, null, " ");
+              }
             }
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true
           }
-        },
-        {
-          type: "string",
-          name: "title",
-          label: "Title",
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: "string",
-          name: "parent",
-          label: "Sub Category",
-          required: false,
-          options: [
-            {
-              value: "Fabrication",
-              label: "Fabrication"
-            },
-            {
-              value: "Multimedia",
-              label: "Multimedia"
-            },
-          ],
-        },
-
-        {
-          type: "rich-text",
-          name: "body",
-          label: "Body",
-          isBody: true,
-        },
-      ],
-    },
-
-    // Wood and Metal Workshop
-    {
-      name: "workshop_equipment",
-      label: "Workshop Equipment",
-      path: "_equipment/workshop",
-      format: "md",
-      defaultItem: () => {
-        return {
-          grand_parent: 'Workshop 202',
-          layout: 'page',
-        }
-      },
-      fields: [
-        {
-          name: "layout",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
-            }
-          }
-        },
-        {
-          name: "grand_parent",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
-            }
-          }
-        },
-        {
-          type: "string",
-          name: "title",
-          label: "Title",
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: "string",
-          name: "parent",
-          label: "Sub Category",
-          required: true,
-          options: [
-            {
-              value: "metal",
-              label: "Metal Working"
-            },
-            {
-              value: "wood",
-              label: "Wood Working"
-            },
-          ],
-        },
-        {
-          type: "rich-text",
-          name: "body",
-          label: "Body",
-          isBody: true,
-        },
-      ],
-    },
-
-    // Fashion
-    {
-      name: "fashion_equipment",
-      label: "Fashion",
-      path: "_equipment/fashion",
-      format: "md",
-      defaultItem: () => {
-        return {
-          parent: 'Fashion 212',
-          layout: 'page',
-        }
-      },
-      fields: [
-        {
-          name: "layout",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
-            }
-          }
-        },
-        {
-          name: "parent",
-          type: "string",
-          ui: {
-            component: () => {
-                return <> </>
-            }
-          }
-        },
-        {
-          type: "string",
-          name: "title",
-          label: "Title",
-          isTitle: true,
-          required: true,
-        },
-        {
-          type: "rich-text",
-          name: "body",
-          label: "Body",
-          isBody: true,
-        },
-      ],
-    },
-    
-  ],
-},
+        ]
+      }
+    ]
+  }
 });
+export {
+  config_default as default
+};
--- .tina/config.tsx
+++ .tina/config.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
