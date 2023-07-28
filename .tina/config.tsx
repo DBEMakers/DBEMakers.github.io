91bdcc42d15017c79dd3d47694e21c3998804161
Amy <116771511+DBEMakers@users.noreply.github.com>
2023-07-28 11:33:28 +0800

    Update config.tsx

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
